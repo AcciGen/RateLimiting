66799cbdc0b2872675100b60b488b3ac08207ea9
AcciGen <145344775+AcciGen@users.noreply.github.com>
2024-05-29 16:00:27 +0500

    Add project files.

---
+++ RateLimiting.UI/Middlewares/RateLimiter.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace RateLimiting.UI.Middlewares
+{
+    public class RateLimiter
+    {
+        private readonly RequestDelegate _next;
+        private readonly IMemoryCache _cache;
+
+        private const int REQUEST_LIMIT = 5;
+
+        public RateLimiter(RequestDelegate next, IMemoryCache cache)
+        {
+            _next = next;
+            _cache = cache;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var keyRequest = "requests";
+            var keyTime = "remaining_time";
+
+            var appends = _cache.Get<int>(keyRequest);
+            var time = _cache.Get<DateTime>(keyTime);
+            var currentTime = DateTime.UtcNow;
+
+            if (appends == 0 && time == DateTime.MinValue)
+            {
+                appends = 1;
+                time = currentTime;
+            }
+
+            else if (time.AddSeconds(30) < currentTime)
+            {
+                time = currentTime;
+                appends = 0;
+            }
+
+            else if (time.AddSeconds(30) > currentTime && appends > REQUEST_LIMIT)
+            {
+                context.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+                await context.Response.WriteAsync("You're reached your rate limit!");
+                return;
+            }
+
+            _cache.Set(keyRequest, ++appends);
+            _cache.Set(keyTime, time);
+
+            await _next(context);
+        }
+
+    }
+}
---
+++ RateLimiting.UI/Program.cs
@@ -0,0 +1,41 @@
+
+using RateLimiting.UI.Middlewares;
+
+namespace RateLimiting.UI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddMemoryCache();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseMiddleware<RateLimiter>();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
